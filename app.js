bffa1ae7d8c84a4622434c7d50c3816c6bbc5b69
Mark Estrada <mark.estrada.dev>
2023-10-04 22:56:02 -0500

    end version updates

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const pauseButton = document.querySelector("#pause")
 const stopButton = document.querySelector("#stop")
 
 const timeDisplay = document.querySelector(".time-display")
+const phaseDisplay = document.querySelector(".phase-display")
 
 // UI Functions
 const updateTimeDisplay = () => {
@@ -34,25 +35,80 @@ const updateTimeDisplay = () => {
   })}`
 }
 
-startButton.addEventListener("click", () => {
-  console.log(timerID)
+const updatePhaseDisplay = () => {
+  phaseDisplay.innerText = phaseTitle
+}
 
+startButton.addEventListener("click", () => {
+  addActiveStyle(startButton)
   timerID = setInterval(() => {
     time -= 1
-    // Zero Occurence
 
+    // Zero Occurence
+    if (time === 0) {
+      clearInterval(timerID)
+      handlePhaseChange()
+      updatePhaseDisplay()
+    }
     updateTimeDisplay()
 
-  }, 100)
+  }, 1000)
 })
 
 pauseButton.addEventListener("click", () => {
+  addActiveStyle(pauseButton)
   clearInterval(timerID)
 })
 
 stopButton.addEventListener("click", () => {
+  addActiveStyle(stopButton)
   clearInterval(timerID)
   time = PHASE_TIME
   updateTimeDisplay()
 })
+
+// HANDLE PHASE CHANGE
+const updatePhase = (newPhaseID, newTime, newTitle) => {
+  time = newTime
+  PHASE_TIME = newTime
+  phaseID = newPhaseID
+  phaseTitle = newTitle
+}
+
+const handlePhaseChange = () => {
+  switch (phaseID) {
+    case 1:
+      updatePhase(2, 300, "Break")
+      break
+    case 2:
+      updatePhase(3, 1500, "Work")
+      break
+    case 3:
+      updatePhase(4, 300, "Break")
+      break
+    case 4:
+      updatePhase(5, 1500, "Work")
+      break
+    case 5:
+      updatePhase(6, 300, "Break")
+      break
+    case 6:
+      updatePhase(7, 1500, "Work")
+      break
+    case 7:
+      updatePhase(8, 1800, "Break")
+      break
+    case 8:
+      updatePhase(1, 1500, "Work")
+      break
+  }
+}
+
+const addActiveStyle = (buttonToUpdate) => {
+  const buttons = [startButton, pauseButton, stopButton]
+  buttons.forEach((button) => {
+    button.classList.remove("active")
+  })
+  buttonToUpdate.classList.add("active")
+}
 
